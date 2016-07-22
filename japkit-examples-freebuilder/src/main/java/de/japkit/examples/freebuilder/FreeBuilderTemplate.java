dcf2b8caaebb3d35a53048912ac0686ea43202e5
Stefan Ocke <stefanocke@users.noreply.github.com>
2016-07-22 15:11:20 +0200

    FreeBuilder example

--- japkit-examples-freebuilder/src/main/java/de/japkit/examples/freebuilder/FreeBuilderTemplate.java
+++ japkit-examples-freebuilder/src/main/java/de/japkit/examples/freebuilder/FreeBuilderTemplate.java
@@ -4,16 +4,23 @@ package de.japkit.examples.freebuilder;
 import de.japkit.annotations.RuntimeMetadata;
 import de.japkit.metaannotations.Field;
 import de.japkit.metaannotations.Getter;
+import de.japkit.metaannotations.InnerClass;
 import de.japkit.metaannotations.Var;
 import de.japkit.metaannotations.classselectors.SrcType;
 
 @RuntimeMetadata
 public class FreeBuilderTemplate {
 	
-	@Var()
-	class interfaze {}; 
+	@Var(fun=SrcType.class)
+	interface Interfaze {}; 
 
 	@Field(src = "#{src.properties}", srcVar = "p",
-			getter = @Getter(commentExpr="the value that will be returned by {@link #{interfaze.asType().simpleName}##{p.getter.simpleName}()}."))
+			getter = @Getter(commentExpr="the value that will be returned by {@link #{interfaze.simpleName}##{p.getter.simpleName}()}."))
 		private SrcType $srcElementName$;
+	
+	@InnerClass
+	static final class Value implements Interfaze {
+		@Field(src = "#{src.properties}", srcVar = "p", getter = @Getter())
+			private SrcType $srcElementName$;
+	}
 }
