b70b690a7699a8fd6c635fbb5b28301ca30f8d03
Stefan Ocke <stefanocke@users.noreply.github.com>
2016-07-27 20:15:20 +0200

    Builder example: equals, hashcode & toString

--- japkit-examples-freebuilder/src/main/java/de/japkit/examples/freebuilder/FreeBuilderTemplate.java
+++ japkit-examples-freebuilder/src/main/java/de/japkit/examples/freebuilder/FreeBuilderTemplate.java
@@ -1,11 +1,15 @@
 package de.japkit.examples.freebuilder;
 
+import java.util.Objects;
+
 import de.japkit.annotations.RuntimeMetadata;
 import de.japkit.metaannotations.CodeFragment;
 import de.japkit.metaannotations.Constructor;
 import de.japkit.metaannotations.Field;
 import de.japkit.metaannotations.Getter;
 import de.japkit.metaannotations.InnerClass;
+import de.japkit.metaannotations.Method;
+import de.japkit.metaannotations.Setter;
 import de.japkit.metaannotations.Var;
 import de.japkit.metaannotations.classselectors.GeneratedClass;
 import de.japkit.metaannotations.classselectors.SrcType;
@@ -16,14 +20,15 @@ public class FreeBuilderTemplate {
 	@Var(fun = SrcType.class)
 	interface Interfaze {
 	};
-	
+
 	@Var(fun = GeneratedClass.class)
 	class Builder {
 	};
 
 	@Field(src = "#{src.properties}",
 			srcVar = "p",
-			getter = @Getter(commentExpr = "the value that will be returned by {@link #{interfaze.simpleName}##{p.getter.simpleName}()}."))
+			getter = @Getter(commentExpr = "the value that will be returned by {@link #{interfaze.simpleName}##{p.getter.simpleName}()}."),
+			setter = @Setter(chain=true, commentExpr = "the value to be returned by {@link #{interfaze.simpleName}##{p.getter.simpleName}()}."))
 	private SrcType $srcElementName$;
 
 	@InnerClass
@@ -32,14 +37,64 @@ public class FreeBuilderTemplate {
 				srcVar = "p",
 				getter = @Getter())
 		private final SrcType $srcElementName$ = null;
-				
-		@CodeFragment(vars = @Var(name="param", expr ="builder.#{src.simpleName}"),
-				code = "this.#{src.simpleName} = #{param};")
-		static class assignment{}
-		
-		@Constructor(bodyIterator = "#{src.properties}", bodyCode = "#{assignment()}")
+
+		@CodeFragment(code = "this.#{src.name} = builder.#{src.name};")
+		static class assignment {
+		}
+
+		@Constructor(bodyIterator = "#{src.properties}",
+				bodyCode = "#{assignment()}")
 		private Value(Builder builder) {
-			
+
+		}
+
+		@CodeFragment(imports = Objects.class,
+				iterator = "#{src.properties}",
+				code = "Objects.equals(#{src.name}, other.#{src.name})",
+				separator = " && ",
+				indentAfterLinebreak = true)
+		class allPropertiesAreEqual {
+		}
+
+		/**
+		 * @japkit.bodyCode
+		 * 
+		 * <pre>
+		 * if (!(obj instanceof Value)) {
+		 * 	return false;
+		 * }
+		 * Value other = (Value) obj;
+		 * return #{allPropertiesAreEqual.code()};
+		 * </pre>
+		 */
+		@Method()
+		@Override
+		public boolean equals(Object obj) {
+			return true;
+		}
+
+		@Method(imports = Objects.class,
+				bodyBeforeIteratorCode = "return Objects.hash(",
+				bodyIterator = "#{src.properties}",
+				bodyCode = "#{src.name}",
+				bodySeparator = ", ",
+				bodyLinebreak = false,
+				bodyAfterIteratorCode = ");")
+		@Override
+		public int hashCode() {
+			return 0;
+		}
+		
+		@Method(bodyBeforeIteratorCode = "return \"#{interfaze.simpleName} {",
+				bodyIterator = "#{src.properties}",
+				bodyCode = "#{src.name}=\" + #{src.name} + \"",
+				bodySeparator = ", ",
+				bodyLinebreak = false,
+				bodyAfterIteratorCode = "}\";")
+		@Override
+		public String toString() {
+			return "";
 		}
 	}
+
 }
