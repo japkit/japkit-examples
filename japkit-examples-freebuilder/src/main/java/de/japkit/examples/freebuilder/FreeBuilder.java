ebda487012dd166b9789b685fb27c93b61ef1c02
Stefan Ocke <Stefan.Ocke@t-systems.com>
2016-08-07 14:22:20 +0200

    Support for Clazz annotation at template class instead of trigger annotation

--- japkit-examples-freebuilder/src/main/java/de/japkit/examples/freebuilder/FreeBuilder.java
+++ japkit-examples-freebuilder/src/main/java/de/japkit/examples/freebuilder/FreeBuilder.java
@@ -3,14 +3,9 @@ package de.japkit.examples.freebuilder;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
 
-import javax.lang.model.element.Modifier;
-
-import de.japkit.metaannotations.Clazz;
-import de.japkit.metaannotations.TemplateCall;
 import de.japkit.metaannotations.Trigger;
 
-
-@Trigger(template=FreeBuilderTemplate.class)
+@Trigger(template = FreeBuilderTemplate.class)
 @Target(ElementType.TYPE)
 public @interface FreeBuilder {
 	boolean shadow() default false;
