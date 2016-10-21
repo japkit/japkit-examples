046424babe815ac33d3589ae25d95bda696f8343
Stefan Ocke <stefanocke@users.noreply.github.com>
2016-10-21 19:06:10 +0200

    Refactoring AnnotationTemplates

--- japkit-examples-roo-petclinic/src/main/java/de/japkit/roo/japkit/meta/AnnotationTemplates.java
+++ japkit-examples-roo-petclinic/src/main/java/de/japkit/roo/japkit/meta/AnnotationTemplates.java
@@ -2,19 +2,9 @@ package de.japkit.roo.japkit.meta;
 
 import java.lang.annotation.Annotation;
 
-import javax.lang.model.element.ElementKind;
-import javax.lang.model.element.Modifier;
-
-import de.japkit.metaannotations.Clazz;
-import de.japkit.metaannotations.InnerClass;
-import de.japkit.metaannotations.TemplateCall;
 import de.japkit.metaannotations.Trigger;
 
-@Trigger(layer=-2)
-@Clazz(modifiers=Modifier.PUBLIC, innerClasses=
-@InnerClass(src="#{triggerAnnotation.targetAnnotations}", modifiers=Modifier.PUBLIC, kind=ElementKind.ANNOTATION_TYPE, 
-	nameExpr="#{src.asElement().simpleName}_",
-	templates={@TemplateCall(AnnotationTemplateAVMembers.class), @TemplateCall(AnnotationTemplateMembers.class)}))
+@Trigger(layer=-2, template=AnnotationTemplatesTemplate.class)
 public @interface AnnotationTemplates {
 	boolean shadow() default false;
 	
