01a46971b0df19d8c5476fede05ad38fe96cee67
Stefan Ocke <stefanocke@users.noreply.github.com>
2016-12-23 10:44:16 +0100

    "real" toString

--- japkit-examples-simpledto/src/main/java/de/japkit/examples/simpledto/DTOTemplate.java
+++ japkit-examples-simpledto/src/main/java/de/japkit/examples/simpledto/DTOTemplate.java
@@ -4,22 +4,29 @@ import de.japkit.annotations.RuntimeMetadata;
 import de.japkit.metaannotations.Clazz;
 import de.japkit.metaannotations.Field;
 import de.japkit.metaannotations.Getter;
+import de.japkit.metaannotations.Method;
 import de.japkit.metaannotations.Setter;
 import de.japkit.metaannotations.classselectors.SrcType;
 
 @Clazz(nameSuffixToAppend = "DTO")
 @RuntimeMetadata
 public class DTOTemplate implements SrcInterface {
-	@Field(src = "#{src.properties}",
+	@Field(src = "#{properties}",
 			getter = @Getter,
 			setter = @Setter)
 	private SrcType $name$;
-	
+
 	/**
-	 * @japkit.bodyCode return "A #{src.simpleName}.";
+	 * <li>japkit.bodyBeforeIteratorCode return "#{name} {"+
+	 * <li>japkit.bodyCode "#{name}=" + #{name} +
+	 * <li>japkit.bodySeparator ", " +
+	 * <li>japkit.bodyAfterIteratorCode "}";
 	 */
+	@Method(bodyIterator = "#{properties}",
+			bodyIndentAfterLinebreak = true)
 	@Override
 	public String toString() {
 		return null;
 	}
+
 }
