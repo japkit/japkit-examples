3f6e151f4ee6d6e2963e17e2b1237c82b10a2994
Stefan Ocke <stefanocke@users.noreply.github.com>
2017-01-02 16:23:51 +0100

    Added SrcClass and SrcInterface in new functions package. Started to deprecate type functions in classselector package.

--- japkit-examples-simpledto/src/main/java/de/japkit/examples/simpledto/DTOTemplate.java
+++ japkit-examples-simpledto/src/main/java/de/japkit/examples/simpledto/DTOTemplate.java
@@ -1,12 +1,13 @@
 package de.japkit.examples.simpledto;
 
 import de.japkit.annotations.RuntimeMetadata;
+import de.japkit.functions.SrcInterface;
+import de.japkit.functions.SrcType;
 import de.japkit.metaannotations.Clazz;
 import de.japkit.metaannotations.Field;
 import de.japkit.metaannotations.Getter;
 import de.japkit.metaannotations.Method;
 import de.japkit.metaannotations.Setter;
-import de.japkit.metaannotations.classselectors.SrcType;
 
 @Clazz(nameSuffixToAppend = "DTO")
 @RuntimeMetadata
--- japkit-examples-simpledto/src/main/java/de/japkit/examples/simpledto/SrcInterface.java
+++
@@ -1,9 +0,0 @@
-package de.japkit.examples.simpledto;
-
-import de.japkit.metaannotations.classselectors.ClassSelector;
-import de.japkit.metaannotations.classselectors.ClassSelectorKind;
-
-@ClassSelector(kind=ClassSelectorKind.SRC_TYPE)
-public interface SrcInterface {
-
-}
